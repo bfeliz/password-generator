6e259bf4fe04cba542017470dd2f934302885b0e
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-02-05 18:34:47 -0700

    Refactor password character selection

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function writePassword() {
     for (let i = 0; i < 1; i++) {
         var userLength = prompt('Pick a number between 8 and 128');
         userInput.push(userLength);
-        console.log(userInput);
         if (userLength < 8 || userLength > 128) {
             alert(
                 'That is not a valid choice, please refresh page to try again'
@@ -29,61 +28,40 @@ function writePassword() {
 
         var userSpecial = confirm('Do you want to use special characters?');
         userInput.push(userSpecial);
-        console.log(userInput);
     }
 
     // create password
-    if (!userUppercase && !userNumbers && !userSpecial) {
-        function generatePassword(length) {
-            var result = '';
-            var characters = 'abcdefghijklmnopqrstuvwxyz';
-            var charactersLength = characters.length;
-            for (var i = 0; i < length; i++) {
-                result += characters.charAt(
-                    Math.floor(Math.random() * charactersLength)
-                );
-            }
-            return result;
+    var passwordArray = [];
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numbers = '1234567890';
+    var special = '!@#$%^&*()';
+
+    if (userLowercase) {
+        passwordArray.push(lowercase);
+    }
+    if (userUppercase) {
+        passwordArray.push(uppercase);
+    }
+    if (userNumbers) {
+        passwordArray.push(numbers);
+    }
+    if (userSpecial) {
+        passwordArray.push(special);
+    }
+
+    var finalString = passwordArray.join('');
+
+    function generatePassword(length) {
+        var result = '';
+        var parameters = finalString;
+        var characterLength = parameters.length;
+        for (var i = 0; i < length; i++) {
+            result += parameters.charAt(
+                Math.floor(Math.random() * characterLength)
+            );
         }
-    } else if (!userNumbers && !userSpecial) {
-        function generatePassword(length) {
-            var result = '';
-            var characters =
-                'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-            var charactersLength = characters.length;
-            for (var i = 0; i < length; i++) {
-                result += characters.charAt(
-                    Math.floor(Math.random() * charactersLength)
-                );
-            }
-            return result;
-        }
-    } else if (!userSpecial) {
-        function generatePassword(length) {
-            var result = '';
-            var characters =
-                'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-            var charactersLength = characters.length;
-            for (var i = 0; i < length; i++) {
-                result += characters.charAt(
-                    Math.floor(Math.random() * charactersLength)
-                );
-            }
-            return result;
-        }
-    } else {
-        function generatePassword(length) {
-            var result = '';
-            var characters =
-                'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
-            var charactersLength = characters.length;
-            for (var i = 0; i < length; i++) {
-                result += characters.charAt(
-                    Math.floor(Math.random() * charactersLength)
-                );
-            }
-            return result;
-        }
+        return result;
     }
 
     console.log(generatePassword(userInput[0]));
